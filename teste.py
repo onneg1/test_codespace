803632d84c217d089e6da5b7dc6f0b65d479efa1
Matheus Teixeira <143418613+onneg1@users.noreply.github.com>
2024-10-31 11:56:45 +0000

    Add colab-1.py

--- teste.py
+++ teste.py
@@ -1,1 +1,22 @@
-print ("teste")
+import os
+
+os.system ("cls || clear")
+
+print ("Colaborador -1")
+
+#COMANDOS DO GIT
+
+# Atualiza o repositório atual com dados do repositório do GitHub
+# > git pull
+
+# Adiciona novos dados para commit.
+# > git add .
+
+# Versiona com os novos dados
+# git commit -m "Título para o commit"
+
+# Atualiza trazendo commits do GitHub
+# git pull
+
+# Envia os commits para o GitHub
+# git push
